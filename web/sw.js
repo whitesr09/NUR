1590df72315d1e154f1f50d680838e526200661f
whitesr09 <96966266+whitesr09@users.noreply.github.com>
2026-08-25 17:33:26 +0530

    Refresh NUR v2 compact motion cache

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
-const CACHE='nur-v3-ui';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='nur-v2-compact-motion-1';
+const ASSETS=['./','./index.html','./styles.css','./v2-refine.css','./app.js','./v2-motion.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
